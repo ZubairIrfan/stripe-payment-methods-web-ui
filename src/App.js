9128cc66158abc0deb681de22953ad38341f9b56
Zubair <zubair.irfan@koderlabs.com>
2023-10-16 13:33:58 +0500

    updated readme.md

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "./components/CheckoutForm"
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './components/CheckoutForm';
 
-const stripePromise = loadStripe("pk_test_51NzK2rJg6T4WRJkVCbhr1Bbd9aHMJkmgLnr77ES0jOEmwxovFEfaVT0pQbzWkFwASkSqKLgyVJwva7sshRR77Ajy00crQgtohc")
+const stripePromise = loadStripe('******'); // stripe publishable key
 
 function App() {
   return (
